02a478821163c32d2f530a03a4b86080c272a66f
bjay kamwa watanabe <bjaywatanabe.kamwa@gmail.com>
2026-03-10 17:04:53 +0100

    Add json response to prompt

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,5 +1,7 @@
+mod checkbox_list;
 mod inline_progress;
 mod two_queue_progress;
 
+pub use checkbox_list::run_checkbox_list;
 pub use inline_progress::{InlineProgress, ProgressHandle};
 pub use two_queue_progress::{TwoQueueProgress, TwoQueueProgressHandle};
